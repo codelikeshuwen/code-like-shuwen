a8ab171c8cfc0627a04d3088ef77c579e6990c6a
Shuwen Wang <shuwen.w@hotmail.com>
2022-01-03 20:49:44 +1100

    first commit

---
+++ public/assets/js/logic.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+  console.log("laogonggong is great");
+  console.log("laopaopao is beautiful");
+  console.error("Moana is cute");
+  console.warn("We need money");
+  console.log(200 + 50 + 50);
+
+  document.querySelector("button#hamburger").onclick = function openMenu() {
+    console.log("I clicked the hamburger!!!!!");
+  };
+});
