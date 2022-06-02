8ffe78fde8b6a6389a98a59d42953d026a760f0d
Shuwen Wang <shuwen.w@hotmail.com>
2022-06-02 10:32:16 +1000

    Add HTML and JS for hamburger menu

--- public/assets/js/logic.js
+++ public/assets/js/logic.js
@@ -1,11 +1,37 @@
-document.addEventListener("DOMContentLoaded", function (event) {
-  console.log("laogonggong is great");
-  console.log("laopaopao is beautiful");
-  console.error("Moana is cute");
-  console.warn("We need money");
-  console.log(200 + 50 + 50);
+function onPageLoad() {
+  console.log("onPageLoad");
+  setupHamburgerMenu();
+  setupEmailSubscriptionForm();
+}
 
-  document.querySelector("button#hamburger").onclick = function openMenu() {
-    console.log("I clicked the hamburger!!!!!");
-  };
-});
+function modalOpen() {}
+
+function setupEmailSubscriptionForm() {
+  console.log("Email form ready");
+  document.querySelector(".signup-boxes button").onclick = getInputValue;
+}
+
+function getInputValue() {
+  console.log("getValue");
+  const email = document.querySelector("footer input").value;
+  const state = document.querySelector("footer select").value;
+  console.log(email);
+  console.log(state);
+  //const email = "jan@jan.com"; // 99 out of 100
+  // let email = "jan@jan.com"; // 1 out of 100
+  // var email = "jan@jan.com";
+
+  //sendToServer(email, state);
+}
+``;
+
+function setupHamburgerMenu() {
+  console.log("Shuwen is beautiful");
+  document.querySelector("button#hamburger").onclick = openHamburgerMenu;
+}
+
+function openHamburgerMenu() {
+  console.log("openHamburgerMenu()");
+}
+
+document.addEventListener("DOMContentLoaded", onPageLoad);
